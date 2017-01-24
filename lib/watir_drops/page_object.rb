ac6af26fc843013f1cf6c7afaa69d171916ccf74
Lu Cui <lu.cui@loblaw.ca>
2017-01-24 16:43:55 -0500

    implement #on_page?

--- lib/watir_drops/page_object.rb
+++ lib/watir_drops/page_object.rb
@@ -2,11 +2,18 @@ module WatirDrops
   class PageObject
     include Watir::Waitable
 
+
+
     class << self
 
       attr_writer :element_list
+      attr_writer :required_element_list
+      attr_reader :require_url
 
-      def page_url
+
+      def page_url(required: false)
+        @require_url = required
+
         define_method("page_url") do |*args|
           yield(*args)
         end
@@ -16,12 +23,24 @@ module WatirDrops
         end
       end
 
+
+      def page_title
+        define_method("page_title") do |*args|
+          yield(*args)
+        end
+      end
+
       def element_list
         @element_list ||= []
       end
 
+      def required_element_list
+        @required_element_list ||= []
+      end
+
       def inherited(subclass)
         subclass.element_list = element_list.dup
+        subclass.required_element_list = required_element_list.dup
       end
 
       def elements(name, &block)
@@ -32,7 +51,7 @@ module WatirDrops
         element_list << name.to_sym
       end
 
-      def element(name, &block)
+      def element(name, required: false, &block)
         define_method(name) do |*args|
           self.instance_exec(*args, &block)
         end
@@ -54,13 +73,17 @@ module WatirDrops
             watir_element.click if val
           end
         end
-
         element_list << name.to_sym
+        required_element_list << name.to_sym if required
       end
 
       def visit(*args)
-        new.tap { |page| page.goto(*args) }
+        new.tap do |page|
+          page.goto(*args)
+          raise Selenium::WebDriver::Error::WebDriverError if page.page_verifiable? && !page.on_page?
+        end
       end
+
 
       def browser=(browser_input)
         @@browser = browser_input
@@ -80,12 +103,12 @@ module WatirDrops
 
     def fill_form(model)
       intersect = case model
-                  when OpenStruct
-                    self.class.element_list & model.to_h.keys
-                  when Hash
-                    self.class.element_list & model.keys
-                  else
-                    self.class.element_list & model.keys.select { |el| !model.send(el).nil? }
+                    when OpenStruct
+                      self.class.element_list & model.to_h.keys
+                    when Hash
+                      self.class.element_list & model.keys
+                    else
+                      self.class.element_list & model.keys.select { |el| !model.send(el).nil? }
                   end
       intersect.each do |val|
         self.send("#{val}=", model[val])
@@ -95,7 +118,30 @@ module WatirDrops
     def inspect
       '#<%s url=%s title=%s>' % [self.class, url.inspect, title.inspect]
     end
+
     alias selector_string inspect
+
+
+    def on_page?
+      raise Selenium::WebDriver::Error::WebDriverError unless page_verifiable?
+
+      @browser.wait_until do |browser|
+        !self.class.require_url || page_url.gsub("#{URI.parse(page_url).scheme}://", '') == browser.url.gsub("#{URI.parse(browser.url).scheme}://", '')
+      end
+
+      @browser.wait_until do |browser|
+        !self.respond_to?(:page_title) || browser.title == page_title
+      end
+
+      @browser.wait_until do |_browser|
+        !self.class.required_element_list.any? || self.class.required_element_list.all? { |e| send(e).present? }
+      end
+
+    true
+
+    rescue Watir::Wait::TimeoutError
+      false
+    end
 
     def method_missing(method, *args, &block)
       if @browser.respond_to?(method)
@@ -107,6 +153,11 @@ module WatirDrops
 
     def respond_to_missing?(method, _include_all = false)
       @browser.respond_to?(method) || super
+    end
+
+    def page_verifiable?
+
+      self.class.require_url || self.respond_to?(:page_title) || self.class.required_element_list.any?
     end
 
   end
---
+++ spec/on_page_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+Watir.default_timeout = 5
+
+class PageRequired < WatirDrops::PageObject
+  page_url(required: true) { 'https://www.google.ca/?gws_rd=ssl' }
+  # page_title { 'Google' }
+  # element(:search) { browser.text_field(id: 'lst-ib') }
+end
+
+class TitleRequired < WatirDrops::PageObject
+  page_url { 'https://www.google.ca' }
+  page_title { 'Google' }
+  element(:search) { browser.text_field(id: 'lst-ib_xxx') }
+end
+
+class ElementsRequired < WatirDrops::PageObject
+  page_url { 'https://www.google.ca/?gws_rd=ssl' }
+  element(:search, required:true) { browser.text_field(id: 'lst-ib') }
+end
+
+class NoneRequired < WatirDrops::PageObject
+  page_url { 'http://bit.ly/watir-webdriver-demo' }
+  element(:search) { browser.text_field(id: 'lst-ib') }
+end
+
+module WatirDrops
+  describe 'PageObject#on_page?' do
+    context 'when url required' do
+      it 'returns true on correct page' do
+        page_required = PageRequired.visit
+        expect(page_required.on_page?).to eql true
+      end
+
+      it 'returns false on incorrect page' do
+        NoneRequired.visit
+        page_required = PageRequired.new
+        expect(page_required.on_page?).to eql false
+      end
+    end
+
+    context 'when title required' do
+      it 'returns true on correct title' do
+        title_required = TitleRequired.visit
+        expect(title_required.on_page?).to eql true
+      end
+
+      it 'returns false on incorrect title' do
+        NoneRequired.visit
+        title_required = TitleRequired.new
+        expect(title_required.on_page?).to eql false
+      end
+    end
+
+    context 'when elements required' do
+      it 'returns true on correct elements' do
+        elements_required = ElementsRequired.visit
+        expect(elements_required.on_page?).to eql true
+      end
+
+      it 'returns false on incorrect elements' do
+        NoneRequired.visit
+        elements_required = ElementsRequired.new
+        expect(elements_required.on_page?).to eql false
+      end
+    end
+
+    context 'when nothing is required' do
+      it 'raises exception' do
+        none_required = NoneRequired.visit
+        expect { none_required.on_page? }.to raise_exception(Selenium::WebDriver::Error::WebDriverError)
+      end
+    end
+  end
+end
