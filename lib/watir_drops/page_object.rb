e08c280f5f2119c1539bf3cd81b98bb33dba31ab
Lu Cui <lu.cui@loblaw.ca>
2017-01-24 16:43:55 -0500

    Added on_page?

--- lib/watir_drops/page_object.rb
+++ lib/watir_drops/page_object.rb
@@ -1,4 +1,4 @@
-require 'active_support/inflector'
+# require 'active_support/inflector'
 
 module WatirDrops
   class PageObject
@@ -7,6 +7,8 @@ module WatirDrops
     class << self
 
       attr_writer :element_list
+      attr_writer :required_element_list
+
 
       def page_url
         define_method("page_url") do |*args|
@@ -18,23 +20,35 @@ module WatirDrops
         end
       end
 
+      def page_title
+        define_method("page_title") do |args|
+          yield(*args)
+        end
+      end
+
       def element_list
         @element_list ||= []
       end
 
-      def inherited(subclass)
-        subclass.element_list = element_list.dup
+      def required_element_list
+        @required_element_list ||= []
       end
 
-      def elements(name, &block)
+      def inherited(subclass)
+        subclass.element_list = element_list.dup
+        subclass.required_element_list = required_element_list.dup
+      end
+
+      def elements(name, required = false, &block)
         define_method(name) do |*args|
           self.instance_exec(*args, &block)
         end
 
         element_list << name.to_sym
+        required_element_list << name.to_sym if required
       end
 
-      def element(name, &block)
+      def element(name, required=false, &block)
         define_method(name) do |*args|
           self.instance_exec(*args, &block)
         end
@@ -42,27 +56,31 @@ module WatirDrops
         define_method("#{name}=") do |val|
           watir_element = self.instance_exec &block
           case watir_element
-          when Watir::Radio
-            watir_element.set if val
-          when Watir::CheckBox
-            val ? watir_element.set : watir_element.clear
-          when Watir::Select
-            watir_element.select val
-          when Watir::Button
-            watir_element.click
-          when Watir::TextField, Watir::TextArea
-            watir_element.set val if val
-          else
-            watir_element.click if val
+            when Watir::Radio
+              watir_element.set if val
+            when Watir::CheckBox
+              val ? watir_element.set : watir_element.clear
+            when Watir::Select
+              watir_element.select val
+            when Watir::Button
+              watir_element.click
+            when Watir::TextField, Watir::TextArea
+              watir_element.set val if val
+            else
+              watir_element.click if val
           end
         end
-
         element_list << name.to_sym
+        required_element_list << name.to_sym if required
       end
 
       def visit(*args)
-        new.tap { |page| page.goto(*args) }
+        new.tap do |page|
+          page.goto(*args)
+          fail "The page is not properly loaded!" unless page.on_page?
+        end
       end
+
 
       def browser=(browser_input)
         @@browser = browser_input
@@ -78,16 +96,17 @@ module WatirDrops
 
     def initialize(browser_input = @@browser)
       @browser = browser_input
+      respond_to?(initialze_root)
     end
 
     def fill_form(model)
       intersect = case model
-                  when OpenStruct
-                    self.class.element_list & model.to_h.keys
-                  when Hash
-                    self.class.element_list & model.keys
-                  else
-                    self.class.element_list & model.keys.select { |el| !model.send(el).nil? }
+                    when OpenStruct
+                      self.class.element_list & model.to_h.keys
+                    when Hash
+                      self.class.element_list & model.keys
+                    else
+                      self.class.element_list & model.keys.select { |el| !model.send(el).nil? }
                   end
       intersect.each do |val|
         self.send("#{val}=", model[val])
@@ -97,7 +116,26 @@ module WatirDrops
     def inspect
       '#<%s url=%s title=%s>' % [self.class, url.inspect, title.inspect]
     end
+
     alias selector_string inspect
+
+
+    def on_page?
+      if self.respond_to?('page_url')
+        Watir::Wait.until { page_url.gsub(/.*:\/\//i,'') == (@browser.url.gsub(/.*:\/\//i,'')) }
+      end
+
+      if self.respond_to?('page_title')
+        Watir::Wait.until { @browser.title == page_title }
+      end
+
+      if self.class.required_element_list.any?
+        fail unless self.class.required_element_list.all? { |e| send(e).present? }
+      end
+
+      true # incase we don't hit any if blocks
+    end
+
 
     def method_missing(method, *args, &block)
       if @browser.respond_to?(method)
