89224250d101d7fc34165d3ee8b4884999695a0d
Titus Fortner <titusfortner@gmail.com>
2017-02-02 12:13:01 -0600

    my alternate implementation of #on_page?

--- lib/watir_drops/page_object.rb
+++ lib/watir_drops/page_object.rb
@@ -11,9 +11,8 @@ module WatirDrops
 
 
       def page_url(required: false)
-        @require_url = true if required
-
         define_method("page_url") do |*args|
+          @require_url = required
           yield(*args)
         end
 
@@ -22,11 +21,9 @@ module WatirDrops
         end
       end
 
-
-      def page_title(required: true)
-        @require_title = true if required
-
+      def page_title
         define_method("page_title") do |*args|
+          @require_title = true
           yield(*args)
         end
       end
@@ -60,18 +57,18 @@ module WatirDrops
         define_method("#{name}=") do |val|
           watir_element = self.instance_exec &block
           case watir_element
-            when Watir::Radio
-              watir_element.set if val
-            when Watir::CheckBox
-              val ? watir_element.set : watir_element.clear
-            when Watir::Select
-              watir_element.select val
-            when Watir::Button
-              watir_element.click
-            when Watir::TextField, Watir::TextArea
-              watir_element.set val if val
-            else
-              watir_element.click if val
+          when Watir::Radio
+            watir_element.set if val
+          when Watir::CheckBox
+            val ? watir_element.set : watir_element.clear
+          when Watir::Select
+            watir_element.select val
+          when Watir::Button
+            watir_element.click
+          when Watir::TextField, Watir::TextArea
+            watir_element.set val if val
+          else
+            watir_element.click if val
           end
         end
         element_list << name.to_sym
@@ -81,7 +78,7 @@ module WatirDrops
       def visit(*args)
         new.tap do |page|
           page.goto(*args)
-          raise Watir::Exception::Error unless page.on_page?
+          raise Watir::Exception::Error if page.verify_page? && !page.on_page?
         end
       end
 
@@ -104,12 +101,12 @@ module WatirDrops
 
     def fill_form(model)
       intersect = case model
-                    when OpenStruct
-                      self.class.element_list & model.to_h.keys
-                    when Hash
-                      self.class.element_list & model.keys
-                    else
-                      self.class.element_list & model.keys.select { |el| !model.send(el).nil? }
+                  when OpenStruct
+                    self.class.element_list & model.to_h.keys
+                  when Hash
+                    self.class.element_list & model.keys
+                  else
+                    self.class.element_list & model.keys.select { |el| !model.send(el).nil? }
                   end
       intersect.each do |val|
         self.send("#{val}=", model[val])
@@ -122,21 +119,27 @@ module WatirDrops
 
     alias selector_string inspect
 
+    def verify_page?
+      page_url if respond_to? :page_url
+      page_title if respond_to? :page_title
+      @require_url || @require_title || !self.class.required_element_list.empty?
+    end
 
     def on_page?
-      begin
-        Watir::Wait.until { page_url.gsub(/.*:\/\//i, '').gsub(/\/$/i, '') == (@browser.url.gsub(/.*:\/\//i, '').gsub(/\/$/i, '')) } if @require_url
-
-        Watir::Wait.until { @browser.title == page_title } if @require_title
-
-        if self.class.required_element_list.any?
-          Watir::Wait.until { self.class.required_element_list.all? { |e| send(e).present? } }
-        end
-      rescue
-        false
-      else
-        true
+      raise Selenium::WebDriver::Error::WebDriverError, "No values present to determine if on desired page" unless verify_page?
+      @browser.wait_until do |browser|
+        !@require_url || page_url.gsub("#{URI.parse(page_url).scheme}://", '') == browser.url.gsub("#{URI.parse(browser.url).scheme}://", '')
       end
+
+      @browser.wait_until { |browser| !@require_title || browser.title == page_title }
+
+      if self.class.required_element_list.any?
+        Watir::Wait.until { self.class.required_element_list.all? { |e| send(e).present? } }
+      end
+
+      true
+    rescue Watir::Wait::TimeoutError
+      false
     end
 
 
@@ -151,6 +154,5 @@ module WatirDrops
     def respond_to_missing?(method, _include_all = false)
       @browser.respond_to?(method) || super
     end
-
   end
 end
